108ea42be601817912dd93138ba1e0431fa7637c
Darek <72472681+DareksCoffee@users.noreply.github.com>
2023-03-26 23:29:19 +0100

    Add files via upload

---
+++ Revolt.CPP/Channel.cpp
@@ -0,0 +1,64 @@
+#include "Channel.h"
+
+#include "Revolt.h"
+#include "static.h"
+
+/**	@param[in]	data	JSON data
+	@param[in]	token	revolt token
+*/
+RevoltCPP::Channel::Channel(const json& data, const std::string& token) : RevoltCPP::RevoltObject(token) {
+    data["id"].get_to<std::string>(id);
+    data["type"].get_to<int>(type);
+    position = get_or_else<int>(data, "position", 0);
+    // permission_overwrites
+    name = get_or_else<std::string>(data, "name", "");
+    icon = get_or_else<std::string>(data, "icon", "");
+}
+
+/**	@param[in]	id		the channel's id
+	@param[in]	token	revolt token
+*/
+RevoltCPP::Channel::Channel(const std::string& id, const std::string& token) : RevoltCPP::RevoltObject(token) {
+    std::string url = "/channels/" + id;
+
+    *this = Channel(api_call(url), token);
+}
+
+/*	@param[in]	old	the Channel to copy
+*/
+RevoltCPP::Channel::Channel(const Channel& old) : RevoltCPP::RevoltObject(old) {
+    id = old.id;
+    type = old.type;
+    position = old.position;
+    //permission_overwrites
+    name = old.name;
+    icon = old.icon;
+}
+
+RevoltCPP::Channel* RevoltCPP::Channel::from_json(Revolt* client, const json& data, const std::string& token) {
+    Channel* channel;
+
+    switch (data.at("type").get<int>()) {
+        case ChannelType::GUILD_TEXT:
+        case ChannelType::GUILD_NEWS:
+            channel = (Channel*)new GuildChannel(data, token);
+            break;
+        case ChannelType::GUILD_VOICE:
+            channel = (Channel*)new VoiceChannel(client, data, token);
+            break;
+        case ChannelType::DM:
+        case ChannelType::GROUP_DM:
+            channel = new DMChannel(data, token);
+            break;
+        default:
+            channel = new Channel(data, token);
+    }
+
+    return channel;
+}
+
+void RevoltCPP::Channel::delete_channel() {
+    std::string url = "/channels/" + id;
+
+    api_call(url, "DEL");
+}
---
+++ Revolt.CPP/User.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+
+#include "RevoltObject.h"
+#include "Logger.h"
+
+namespace RevoltCPP {
+
+class Message;
+class Embed;
+class DMChannel;
+
+    ///ther user's name
+    std::string username;
+    ///the user's avatar hash
+    std::string avatar;
+    ///true, if the user is a bot
+    bool bot = false;
+    ///true, if the user has enabled two factor authentification
+    bool mfa_enabled = false;
+    ///the user's chosen language option
+    std::string locale;
+    ///true, if the user's email has been verified
+    bool verified = false;
+    ///the user's email
+    std::string email;
+    ///the flags on a user's account
+    int flags = 0;
+
+    DLL_EXPORT User() {}
+    DLL_EXPORT User(const json& data, const std::string& token);
+    DLL_EXPORT User(const std::string& id, const std::string& token);
+    DLL_EXPORT virtual ~User() {}
+
+    ///get/create the DMChannel for this user
+    DLL_EXPORT DMChannel get_dmchannel();
+    ///send a message to this user
+    DLL_EXPORT Message send(const std::string& content, const bool tts = false);
+    ///send an embed to this user
+    DLL_EXPORT Message send(const Embed& embed);
+
+    ///@return Username as std::string
+    DLL_EXPORT virtual operator std::string() { return username; };
+};
